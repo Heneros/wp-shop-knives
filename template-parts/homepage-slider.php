86f23bb25584ffd3d4ebd75652e55d9740885632
Heneros <punk60826@gmail.com>
2022-11-28 01:19:17 +0200

    template-parts homepage.slider.php

---
+++ template-parts/homepage-slider.php
@@ -0,0 +1,91 @@
+<section class="slider-homepage">
+    <div class="container">
+        <div class="slider-homepage__block">
+            <div class="slider-homepage__swiper swiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="slider-homepage__text-content">
+                            <h1 data-aos="fade-down" class="slider-homepage__title">Интернет магазин уникальных стальных
+                                ножей
+                            </h1>
+                            <p data-aos="fade-down" class="slider-homepage__descr">Lorem, ipsum dolor sit amet
+                                consectetur adipisicing elit.
+                                At libero nesciunt consequuntur voluptate sint saepe voluptatibus repudiandae incidunt
+                                perferendis magni aperiam eum odio nam sequi impedit, voluptates quos, accusamus maxime,
+                                iste quaerat corrupti molestias in.</p>
+                            <button data-aos="fade-down" class="slider-homepage__btn btn">Подробнее</button>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="slider-homepage__text-content">
+                            <h1 data-aos="fade-down" class="slider-homepage__title">Интернет магазин уникальных стальных
+                                ножей
+                            </h1>
+                            <p data-aos="fade-down" class="slider-homepage__descr">Lorem, ipsum dolor sit amet
+                                consectetur adipisicing elit.
+                                At libero nesciunt
+                                consequuntur voluptate sint saepe voluptatibus repudiandae incidunt perferendis magni
+                                aperiam eum odio nam sequi
+                                impedit, voluptates quos, accusamus maxime, iste quaerat corrupti molestias in.</p>
+                            <button data-aos="fade-down" class="slider-homepage__btn btn">Подробнее</button>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="slider-homepage__text-content">
+                            <h1 data-aos="fade-down" class="slider-homepage__title">Интернет магазин уникальных стальных
+                                ножей
+                            </h1>
+                            <p data-aos="fade-down" class="slider-homepage__descr">Lorem, ipsum dolor sit amet
+                                consectetur adipisicing elit.
+                                At libero nesciunt
+                                consequuntur voluptate sint saepe voluptatibus repudiandae incidunt perferendis magni
+                                aperiam eum odio nam sequi
+                                impedit, voluptates quos, accusamus maxime, iste quaerat corrupti molestias in.</p>
+                            <button data-aos="fade-down" class="slider-homepage__btn btn">Подробнее</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider-homepage__navigation">
+                    <div class="slider-homepage__pagination swiper-pagination"></div>
+                    <div class="slider-homepage__scrollbar swiper-scrollbar"></div>
+                </div>
+            </div>
+            <div class="slider-homepage__product-image">
+                <div class="slider-homepage__product-block">
+                    <div class="slider-homepage__plus-one"></div>
+                    <div class="slider-homepage__plus-two"></div>
+                    <div class="slider-homepage__plus-three"></div>
+                    <img src="<?php echo _assets_paths('/img/homepage-product-img.png'); ?>" alt="homepage image">
+                </div>
+            </div>
+
+        </div>
+
+        <div data-aos="fade-up" class="slider-homepage__icons">
+            <div class="slider-homepage__icon">
+                <div class="slider-homepage__icon-block">
+                    <img src="img/sprite.svg#guarantee" alt="icon guarantee">
+                </div>
+                <span>С другой стороны укрепление и развитие структуры.</span>
+            </div>
+            <div class="slider-homepage__icon">
+                <div class="slider-homepage__icon-block">
+                    <img src="img/sprite.svg#delivery" alt="icon delivery">
+                </div>
+                <span>С другой стороны укрепление и развитие структуры </span>
+            </div>
+            <div class="slider-homepage__icon">
+                <div class="slider-homepage__icon-block">
+                    <img src="img/sprite.svg#clearance" alt="icon clearance">
+                </div>
+                <span>С другой стороны укрепление и позиции влечет за собой</span>
+            </div>
+            <div class="slider-homepage__icon">
+                <div class="slider-homepage__icon-block">
+                    <img src="img/sprite.svg#discount" alt="icon discount">
+                </div>
+                <span>С другой позиции влечет за собой и развитие структуры.</span>
+            </div>
+        </div>
+    </div>
+</section>
--- templates/homepage.php
+++ templates/homepage.php
@@ -10,5 +10,11 @@ get_header();
 ?>
 
 
+<?php
+
+///Slider Homepage
+get_template_part('template-parts/homepage-slider');
+
+?>
 
 <?php get_footer(); ?>
