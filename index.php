c0629b16f9b999e129f2c6778e5ae76b29852344
Heneros <punk60826@gmail.com>
2022-12-09 04:32:57 +0200

    tags display

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 get_header();
 
 ?>
-Test
 
 
 <?php get_footer(); ?>
--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -61,18 +61,20 @@ if (empty($product) || !$product->is_visible()) {
 		<div class="bestsellers-products-item__top">
 			<?php
 			do_action('woocommerce_shop_loop_item_title');
+		
 			?>
 		</div>
 
 		<div class="bestsellers-products-item__bottom">
 			<?php
+
 			/**
 			 * Hook: woocommerce_after_shop_loop_item_title.
 			 *
 			 * @hooked woocommerce_template_loop_rating - 5
 			 * @hooked woocommerce_template_loop_price - 10
 			 */
-			
+
 			do_action('woocommerce_after_shop_loop_item_title');
 			echo print_wish_icon($product->get_id());
 			/**
