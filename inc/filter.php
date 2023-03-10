2d638bfc519678496a671acda2e322011003fa45
Heneros <punk60826@gmail.com>
2023-03-10 22:57:28 +0200

    filter template updated

--- inc/filter.php
+++ inc/filter.php
@@ -57,15 +57,62 @@ function projects_post_type()
         'meta_box_cb' => 'post_categories_meta_box',
         'show_admin_column' => true
     ]);
+
+    register_taxonomy('project_areas', ['projects'], [
+        'labels' => [
+            'name' => __('Project Areas'),
+            'singular_name' => __('Project Areas'),
+            'menu_name'     => __('Project Areas '),
+            'search_items'      => __('Serch Project Areas'),
+            'parent_item'  => __('Parent Project Areas:'),
+            'parent_item_colon' => __('Parent Project Areas:'),
+            'all_items'         => __('All Project Areas'),
+            'update_item'       => __('Update Project Area'),
+            'add_new_item'     => __('Add new Project Area'),
+            'edit_item'         => __('Edit Project Area'),
+            'new_item_name'     => __('New name Project Area'),
+        ],
+        'public' => true,
+        'show_in_rest' => true,
+        'hierarchical'          => true,
+        'update_count_callback' => '_update_post_term_count',
+        'rewrite' => ['slug' => 'projects_areas'],
+        'meta_box_cb' => 'post_categories_meta_box',
+        'show_admin_column' => true
+    ]);
+    register_taxonomy('field_activities', ['projects'], [
+        'labels' => [
+            'name' => __('Field Activity'),
+            'singular_name' => __('Field Activity'),
+            'menu_name'     => __('Field Activity'),
+            'search_items'      => __('Serch Field Activities'),
+            'parent_item'  => __('Parent Field Activity'),
+            'parent_item_colon' => __('Parent Field Activity'),
+            'all_items'         => __('All Field Activity'),
+            'update_item'       => __('Update Field Activity '),
+            'add_new_item'     => __('Add new Field Activity '),
+            'edit_item'         => __('Edit Field Activity '),
+            'new_item_name'     => __('New name Field Activity '),
+        ],
+        'public' => true,
+        'show_in_rest' => true,
+        'hierarchical'          => true,
+        'update_count_callback' => '_update_post_term_count',
+        'rewrite' => ['slug' => 'field_activities'],
+        'meta_box_cb' => 'post_categories_meta_box',
+        'show_admin_column' => true
+    ]);
 }
+
 
 
 
 add_action('filters_tax_projects', 'functions_tax_projects');
 
+
 function functions_tax_projects()
 {
-    echo get_projects_filter_form(array('project_areas'));
+    echo get_projects_filter_form(array('project_areas', 'field_activities'));
 }
 
 
@@ -74,7 +121,7 @@ function get_projects_filter_form($taxonomies = array())
     $form = '<form method="get" action="' . esc_url(get_permalink()) . '">';
 
     foreach ($taxonomies as $taxonomy) {
-        $current_term = isset($_GET[$taxonomy]) ? sanitize_text_field($_GET[$taxonomy]) : '';
+        $current_terms = isset($_GET[$taxonomy]) ? (array) $_GET[$taxonomy] : array();
         $args = array(
             'taxonomy' => $taxonomy,
             'hide_empty' => false,
@@ -85,12 +132,12 @@ function get_projects_filter_form($taxonomies = array())
             $form .= '<span>' . esc_html(get_taxonomy($taxonomy)->labels->singular_name) . '</span>';
             foreach ($terms as $term) {
                 $checked = '';
-                if ($current_term === $term->slug) {
+                if (in_array($term->slug, $current_terms)) {
                     $checked = 'checked';
                 }
                 $form .= '<div>';
                 $form .= '<label for="' . esc_attr($taxonomy . '_' . $term->slug) . '">';
-                $form .= '<input type="radio" name="' . esc_attr($taxonomy) . '" id="' . esc_attr($taxonomy . '_' . $term->slug) . '" value="' . esc_attr($term->slug) . '" ' . esc_attr($checked) . '>';
+                $form .= '<input type="checkbox" name="' . esc_attr($taxonomy) . '[]" id="' . esc_attr($taxonomy . '_' . $term->slug) . '" value="' . esc_attr($term->slug) . '" ' . esc_attr($checked) . ' onchange="this.form.submit();">';
                 $form .= esc_html($term->name);
                 $form .= '</label>';
                 $form .= '</div>';
@@ -100,7 +147,7 @@ function get_projects_filter_form($taxonomies = array())
     }
 
     $form .= '<input type="hidden" name="page_id" value="' . esc_attr(get_the_ID()) . '">';
-    $form .= '<button type="submit">Filter</button>';
+
     $form .= '</form>';
 
     return $form;
--- templates/filter.php
+++ templates/filter.php
@@ -11,44 +11,90 @@ get_header();
 
 
 <?php
-do_action('filters_tax_projects');
 
-$taxonomies = array('project_areas', 'project_types');
-$form = get_projects_filter_form($taxonomies);
+
+
+
 
 $args = array(
     'post_type' => 'projects',
+    'posts_per_page' => 5,
 );
-if (!empty($_GET['project_areas'])) {
-    $selected_term = sanitize_text_field($_GET['project_areas']);
-    if (!empty($selected_term)) {
-        $args['tax_query'] = array(
-            array(
-                'taxonomy' => 'project_areas',
-                'field'    => 'slug',
-                'terms'    => $selected_term,
-            ),
-        );
-    }
-}
-
-
-
-
-
-
-
-
-
-$query = new WP_Query($args);
-if ($query->have_posts()) :
-    while ($query->have_posts()) :
-        $query->the_post();
-        the_title();
-    endwhile;
-endif;
-wp_reset_postdata();
 ?>
+<div class="container">
+    <?php
+
+
+    do_action('filters_tax_projects');
+    if (!empty($_GET['project_areas']) || !empty($_GET['field_activities'])) {
+        $tax_query = array('relation' => 'AND');
+
+        if (!empty($_GET['project_areas'])) {
+            $selected_terms = is_array($_GET['project_areas']) ? $_GET['project_areas'] : array($_GET['project_areas']);
+            $tax_query[] = array(
+                'taxonomy' => 'project_areas',
+                'field' => 'slug',
+                'terms' => $selected_terms,
+            );
+        }
+
+        if (!empty($_GET['field_activities'])) {
+            $selected_terms = is_array($_GET['field_activities']) ? $_GET['field_activities'] : array($_GET['field_activities']);
+            $tax_query[] = array(
+                'taxonomy' => 'field_activities',
+                'field' => 'slug',
+                'terms' => $selected_terms,
+            );
+        }
+
+        $args['tax_query'] = $tax_query;
+    }
+
+    ?>
+
+    <div class="items-project">
+        <?php
+        $query = new WP_Query($args);
+        if ($query->have_posts()) :
+            while ($query->have_posts()) :
+                $query->the_post();
+                global $post;
+                $featured_img = wp_get_attachment_url(get_post_thumbnail_id());
+                $link = get_permalink();
+                $field_activities_tax = get_the_term_list($post->ID, 'field_activities', '', ', ');
+                $fields_active =   strip_tags($field_activities_tax);
+                $fields_active = '<span>' . $fields_active . '</span>';
+
+
+                $project_areas_tax = get_the_term_list($post->ID, 'project_areas', '', ', ');
+                ///  get_the_term_list( $post->ID, 'project_areas', ' ', ', ' ); 
+        ?>
+
+                <div class="cardd-item">
+                    <div class="card-img" style="background-image: url(<?php echo $featured_img  ?>); ">
+                        <span class=""></span>
+                    </div>
+                    <div class="card-content">
+                        <h1>
+                            <a href="<?= $link ?>">
+                                <?php the_title(); ?>
+                            </a>
+                        </h1>
+                        <div class="card-bottom">
+                            <?php
+                            echo $fields_active
+                            ?>
+                        </div>
+                    </div>
+                </div>
+
+        <?php
+            endwhile;
+        endif;
+        wp_reset_postdata();
+        ?>
+    </div>
+</div>
 
 <?php
 get_footer();
