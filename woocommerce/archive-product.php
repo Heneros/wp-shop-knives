edb11b572af02d3297445964ccc853e092d192e6
Heneros <punk60826@gmail.com>
2023-04-10 00:08:53 +0300

    archive-product

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The Template for displaying product archives, including the main shop page which is a post type archive
  *
@@ -15,9 +16,9 @@
  * @version 3.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-get_header( 'shop' );
+get_header('shop');
 
 /**
  * Hook: woocommerce_before_main_content.
@@ -26,11 +27,11 @@ get_header( 'shop' );
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-do_action( 'woocommerce_before_main_content' );
+do_action('woocommerce_before_main_content');
 
 ?>
 <header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+	<?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
 		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 	<?php endif; ?>
 
@@ -41,65 +42,112 @@ do_action( 'woocommerce_before_main_content' );
 	 * @hooked woocommerce_taxonomy_archive_description - 10
 	 * @hooked woocommerce_product_archive_description - 10
 	 */
-	do_action( 'woocommerce_archive_description' );
+	do_action('woocommerce_archive_description');
 	?>
 </header>
+<section class="shop-category">
+	<div class="container">
+		<div class="shop-category__information">
+			<h1 class="shop-category__title title">Blade Knifes</h1>
+			<div class="shop-category__second-row">
+				<div class="shop-category__breadcrumbs">
+					<a href="index.html" class="breadcrumbs-link breadcrumbs-homepage">Homepage</a>
+					<span class="divide">></span>
+					<a href="shop.html" class="breadcrumbs-link">Blade Knifes</a>
+				</div>
+				<button class="btn btn-shop" id="btn-filters">
+					Filters
+				</button>
+				<div class="shop-category__sortby">
+					<select class="filter-style select-item">
+						<option value="">Popularity</option>
+						<option value="">Newness</option>
+						<option value="">Rating</option>
+					</select>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="shop-catalog">
+	<div class="container grid">
+		<div class="shop-catalog__left">
+			<div class="blank"></div>
+		</div>
+		<div class="shop-catalog__right">
+
+			<?php
+			if (woocommerce_product_loop()) {
+
+				/**
+				 * Hook: woocommerce_before_shop_loop.
+				 *
+				 * @hooked woocommerce_output_all_notices - 10
+				 * @hooked woocommerce_result_count - 20
+				 * @hooked woocommerce_catalog_ordering - 30
+				 */
+				// do_action('woocommerce_before_shop_loop');
+
+				// woocommerce_product_loop_start();
+			?>
+
+				<div class="shop-catalog__products">
+					<?php
+					if (wc_get_loop_prop('total')) {
+						while (have_posts()) {
+							the_post();
+
+							/**
+							 * Hook: woocommerce_shop_loop.
+							 */
+							// do_action('woocommerce_shop_loop');
+					?>
+							<div class="bestsellers-products-item">
+								<?php
+								wc_get_template_part('content', 'product');
+								?>
+							</div>
+				<?php
+						}
+					}
+
+					woocommerce_product_loop_end();
+
+					/**
+					 * Hook: woocommerce_after_shop_loop.
+					 *
+					 * @hooked woocommerce_pagination - 10
+					 */
+					do_action('woocommerce_after_shop_loop');
+				} else {
+					/**
+					 * Hook: woocommerce_no_products_found.
+					 *
+					 * @hooked wc_no_products_found - 10
+					 */
+					do_action('woocommerce_no_products_found');
+				}
+
+				?>
+				</div>
+				<?php
+				/**
+				 * Hook: woocommerce_after_main_content.
+				 *
+				 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+				 */
+				do_action('woocommerce_after_main_content');
+
+				/**
+				 * Hook: woocommerce_sidebar.
+				 *
+				 * @hooked woocommerce_get_sidebar - 10
+				 */
+				?>
+
+		</div>
+
+	</div>
+</section>
 <?php
-if ( woocommerce_product_loop() ) {
-
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action( 'woocommerce_before_shop_loop' );
-
-	woocommerce_product_loop_start();
-
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
-			the_post();
-
-			/**
-			 * Hook: woocommerce_shop_loop.
-			 */
-			do_action( 'woocommerce_shop_loop' );
-
-			wc_get_template_part( 'content', 'product' );
-		}
-	}
-
-	woocommerce_product_loop_end();
-
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
-} else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
-}
-
-/**
- * Hook: woocommerce_after_main_content.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
- */
-do_action( 'woocommerce_after_main_content' );
-
-/**
- * Hook: woocommerce_sidebar.
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action( 'woocommerce_sidebar' );
-
-get_footer( 'shop' );
+get_footer('shop');
